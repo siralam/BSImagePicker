45dbe65add77b295d7f27c79f3bdc93508b4e1af
Sira Lam <sira.lam@gravitas.com.hk>
2018-02-27 17:15:40 +0800

    Created project

---
+++ app/src/main/java/com/asksira/imagepickersheetdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.asksira.imagepickersheetdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.entrance).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
