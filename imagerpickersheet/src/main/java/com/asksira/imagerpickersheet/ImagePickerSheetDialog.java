10c18e61fc5b4239423ed9ce50b39b387a9b8915
Sira Lam <sira.lam@gravitas.com.hk>
2018-03-01 10:37:59 +0800

    Improved the way of anchoring bottom bar by setting the Z translation correctly

--- imagerpickersheet/src/main/java/com/asksira/imagerpickersheet/ImagePickerSheetDialog.java
+++ imagerpickersheet/src/main/java/com/asksira/imagerpickersheet/ImagePickerSheetDialog.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 /**
  * This is the core class of this library, which extends BottomSheetDialogFragment
@@ -60,8 +61,10 @@ public class ImagePickerSheetDialog extends BottomSheetDialogFragment {
     }
 
     private void setupBottomBar (View rootView) {
-        FrameLayout parentView = (FrameLayout)(rootView.getParent().getParent().getParent());
-        parentView.addView(LayoutInflater.from(getContext()).inflate(R.layout.item_selection_bar, parentView, false), -1);
+        CoordinatorLayout parentView = (CoordinatorLayout) (rootView.getParent().getParent());
+        View barView = LayoutInflater.from(getContext()).inflate(R.layout.item_selection_bar, parentView, false);
+        ViewCompat.setTranslationZ(barView, ViewCompat.getZ((View)rootView.getParent()));
+        parentView.addView(barView, -1);
     }
 
     /**
